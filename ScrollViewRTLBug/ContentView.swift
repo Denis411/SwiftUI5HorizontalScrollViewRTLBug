624c88de4f2dd458291a5673f87e3f87b4e9aeac
Denis Ulianov <dulianov@tradingview.com>
2023-11-28 11:12:55 +0400

    Reproduce SwiftUI 5 RTL bug

--- ScrollViewRTLBug/ContentView.swift
+++ ScrollViewRTLBug/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
+struct Element: Identifiable {
+    var id = UUID()
+    let value: String
+
+    init(_ value: Int) {
+        self.value = "Element \(value)"
+    }
+}
+
+let bubbles: [Element] = {
+    var array: [Element] = []
+    for num in 0...5 {
+        array.append(Element(num))
+    }
+    return array
+}()
+
 struct ContentView: View {
+    let elements = bubbles
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(elements) { element in
+                    Text(element.value)
+                        .padding(5)
+                        .background(Color.yellow)
+                        .cornerRadius(5)
+                }
+            }
         }
-        .padding()
+        .environment(\.layoutDirection, .rightToLeft)
     }
 }
 
