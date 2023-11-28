4b4194ec3dcdb5c52872490d017856f25ce8e1af
Denis Ulianov <dulianov@tradingview.com>
2023-11-28 10:37:39 +0400

    Initial Commit

---
+++ ScrollViewRTLBug/ScrollViewRTLBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewRTLBugApp.swift
+//  ScrollViewRTLBug
+//
+//  Created by dulianov on 28.11.23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewRTLBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
